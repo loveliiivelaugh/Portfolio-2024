ca6ba9e77d4af003d2958a37483897853f4d8747
M <woodward.michael.a@gmail.com>
2024-02-21 22:13:06 -0600

    New layout

---
+++ src/utilities/cms.js
@@ -0,0 +1,32 @@
+export const cms = {
+    app: {
+        blog: {
+            posts: [
+                {
+                    title: "Building myFitness",
+                    body: `The purpose for building myFitness was to build an app that I can use on a daily basis to replace some of the more main stream apps that are available in that space.
+                    The reason for doing this was to be able to store my own data in my own database as well as include additional amd custom features to the app.
+                    A future goal for the app is to continue to use its foundation and architecture but integrate with other services such as some of the emerging AI technologies and tools becoming available every day.`,
+                },
+                {
+                    title: "Building FamilyApps",
+                    body: `The purpose for building myFitness was to build an app that I can use on a daily basis to replace some of the more main stream apps that are available in that space.
+                    The reason for doing this was to be able to store my own data in my own database as well as include additional amd custom features to the app.
+                    A future goal for the app is to continue to use its foundation and architecture but integrate with other services such as some of the emerging AI technologies and tools becoming available every day.`,
+                },
+                {
+                    title: "Working with AI",
+                    body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+                },
+                {
+                    title: "Automating the Way Forward",
+                    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                },
+                {
+                    title: "Tech Work in the Modern World",
+                    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                },
+            ]
+        }
+    }
+}
