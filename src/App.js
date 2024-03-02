291efc2be96a3c35893800cd77307a7f6cb7ddf2
M <woodward.michael.a@gmail.com>
2024-03-02 13:46:50 -0600

    Added links to projects and socials

--- src/App.js
+++ src/App.js
@@ -1,73 +1,262 @@
-
+import { useState } from 'react';
+import Markdown from 'react-markdown'
 import { 
   Box, Button, Grid, Typography,
   Container, Divider,
-  Avatar, List, ListItem, ListItemText, Toolbar, IconButton
+  Modal,
+  Avatar, List, ListItem, ListItemText, Toolbar, IconButton, Tooltip, ListItemButton, TextField
 } from '@mui/material'
-import {
-  Timeline, TimelineItem, TimelineSeparator, TimelineConnector, 
-  TimelineContent, TimelineDot
-} from '@mui/lab'
-import { FaLinkedin } from "react-icons/fa";
 import TextSnippetIcon from '@mui/icons-material/TextSnippet';
+import LaunchIcon from '@mui/icons-material/Launch';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { FaLinkedin } from "react-icons/fa";
+import CloseIcon from '@mui/icons-material/Close';
 
 import { cms } from './utilities/cms';
 import headshot from './assets/images/headshot-cropped.png'
-// import './App.css';
 
 
-const workHistory = [ 
-  { 
-    name: "MedPro",
-    role: "Salesforce Administrator",
-    period: "January 2019 - March 2020",
-    responsibilities: "Admin responsibilities"
-  },
-  { 
-    name: "3vue",
-    role: "Front End React Developer",
-    period: "June 2021 - April 2022",
-    responsibilities: "Build and maintain dashboards for business intelligence solutions."
-  }, 
-  { 
-    name: "Charter",
-    role: "JavaScript Developer II",
-    period: "June 2022 - Current",
-    responsibilities: "JavaScript developer working in Automation department. Projects focused on frontend development mostly with React and Angular. Also working on backend development with NodeJS and Python."
-  },
-]
 
-function AlternateReverseTimeline() {
-  return (
-    <Timeline position="alternate-reverse">
-    {workHistory.map(work => (
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Typography>{work.name}</Typography>
-          <Typography>{work.role}</Typography>
-          <Typography>{work.period}</Typography>
-        </TimelineContent>
-      </TimelineItem>
-    ))}
-    </Timeline>
-  );
-}
+
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  borderRadius: 4,
+  boxShadow: 24,
+  p: 4,
+};
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedPost, setSelectedPost] = useState(null);
+  const [form, setForm] = useState({})
+
+  const handleReadMore = (post) => {
+    setSelectedPost(post);
+    setModalOpen(true);
+  };
+
+  const handleSchedule = () => {
+    setModalOpen(true);
+    setSelectedPost({ schedule_form: true });
+  };
+
+  const handleCloseModal = () => {
+    setSelectedPost(null);
+    setModalOpen(false);
+  };
+
+  const handleChange = e => setForm(prev => ({ ...prev, [e.target.id]: e.target.value }));
+
+  const formData = [
+    {
+        id: 'name',
+        key: 'name',
+        label: 'Your Name',
+        variant: 'outlined',
+        value: form['name'],
+        onChange: handleChange,
+        placeholder: 'Enter your name',
+        type: 'input',
+    },
+    {
+        id: 'email',
+        key: 'email',
+        label: 'Email address',
+        variant: 'outlined',
+        value: form['email'],
+        onChange: handleChange,
+        placeholder: 'Enter your email address',
+        type: 'input',
+    },
+    {
+        id: 'companyName',
+        key: 'companyName',
+        label: 'Company Name',
+        variant: 'outlined',
+        value: form['companyName'],
+        onChange: handleChange,
+        placeholder: 'Enter your company name',
+        type: 'input',
+    },
+    {
+        id: 'companyUrl',
+        key: 'companyUrl',
+        label: 'Company URL',
+        variant: 'outlined',
+        value: form['companyUrl'],
+        onChange: handleChange,
+        placeholder: 'Enter your company URL',
+        type: 'input',
+    },
+    {
+        id: 'servicesNeeded',
+        key: 'servicesNeeded',
+        label: 'What Services Do You Need?',
+        variant: 'outlined',
+        value: form['servicesNeeded'],
+        onChange: handleChange,
+        placeholder: 'Enter the services needed',
+        type: 'input',
+    },
+    {
+        id: 'budget',
+        key: 'budget',
+        label: 'Our Budget is',
+        variant: 'outlined',
+        value: form['budget'],
+        onChange: handleChange,
+        placeholder: 'Enter your budget',
+        type: 'input',
+    },
+    {
+        id: 'copywriteStatus',
+        key: 'copywriteStatus',
+        label: 'What is your copywrite status',
+        variant: 'outlined',
+        value: form['copywriteStatus'],
+        onChange: handleChange,
+        placeholder: 'Enter your copywrite status',
+        type: 'input',
+    },
+    {
+        id: 'designStatus',
+        key: 'designStatus',
+        label: 'What is your design status',
+        variant: 'outlined',
+        value: form['designStatus'],
+        onChange: handleChange,
+        placeholder: 'Enter your design status',
+        type: 'input',
+    },
+    {
+        id: 'deadline',
+        key: 'deadline',
+        label: 'Our deadline is',
+        variant: 'outlined',
+        value: form['deadline'],
+        onChange: handleChange,
+        placeholder: 'Enter your deadline',
+        type: 'input',
+    },
+    {
+        id: 'description',
+        key: 'description',
+        label: 'Project Description',
+        variant: 'outlined',
+        value: form['description'],
+        onChange: handleChange,
+        placeholder: 'Give a brief explanation of the project in mind',
+        type: 'input',
+    },
+];
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log("Submitted Form: ", form);
+
+  const link = document.createElement("a");
+  link.href="https://calendly.com/woodward-michael-a/15min"
+  link.target='_blank'
+  link.rel="noreferrer"
+  link.click();
+
+  // Send form data to server to send email with data
+  setForm({});
+
+  setModalOpen(false);
+}
+
   return (
     <>
+
+    <Modal open={modalOpen}>
+      <Box sx={style}>
+        <Toolbar sx={{ justifyContent: "end" }}>
+          <IconButton onClick={handleCloseModal}>
+            <CloseIcon />
+          </IconButton>
+        </Toolbar>
+
+        {selectedPost?.schedule_form
+          ? (
+            <Box component="form" onSubmit={handleSubmit}>
+              <Typography variant="h4" gutterBottom>
+                Project Details
+              </Typography>
+              <Grid container spacing={2} mb={2}>
+                {formData.map(fieldProps => (
+                  <Grid item xs={12}>
+                    <TextField {...fieldProps} fullWidth />
+                  </Grid>
+                ))}
+              </Grid>
+              <Divider />
+              <Box sx={{ display: "flex", justifyContent: "right", gap: 2, mt: 2 }}>
+                <Button variant="outlined" color="primary" onClick={() => setModalOpen(false)}>
+                  Back
+                </Button>
+                <Button variant="contained" color="primary" type={"submit"}>
+                  Confirm
+                </Button>
+              </Box>
+            </Box>
+          ) : (
+            <>
+              <Box sx={{ textAlign: "center" }}>
+                <Typography variant="h4" gutterBottom>
+                  {selectedPost?.title}
+                </Typography>
+                {selectedPost?.image && (
+                  <img 
+                    src={selectedPost?.image} 
+                    alt="project thumbnail" 
+                    style={{ 
+                      width: 160, 
+                      height: 320, 
+                      border: '8px solid rgba(0, 0, 0, 1)', 
+                      borderRadius: "10px" 
+                    }} 
+                  />
+                )}
+              </Box>
+              <Markdown>
+                {selectedPost?.body}
+              </Markdown>
+              {selectedPost?.type.includes("project") && (
+                <Box sx={{ textAlign: "right" }}>
+                  <Tooltip title="Source Code">
+                    <IconButton href={selectedPost?.github} target='_blank' noreferrer>
+                      <GitHubIcon />
+                    </IconButton>
+                  </Tooltip>
+                  <Tooltip title="View Demo">
+                    <IconButton href={selectedPost?.url} target='_blank' noreferrer>
+                      <LaunchIcon />
+                    </IconButton>
+                  </Tooltip>
+                </Box>
+              )}
+            </>
+          )}
+      </Box>
+    </Modal>
+
     <Box sx={{ minHeight: "100vh" }}>
       <Box sx={{ height: '24vh', width: '100%', background: '#232323' }}>
         {/* Hero Section */}
         <Typography variant="h4" sx={{ color: "#Fff", px: 2, pt: 6 }}>
-          Woodward Software
+          Woodward Digital Services
         </Typography>
         <Typography variant="body1" sx={{ color: "#Fff", px: 2 }}>
-          Automation. AI Integration. Mobile Web App Development.
+          Web Design and Development
         </Typography>
       </Box>
       <List size="small">
@@ -76,35 +265,25 @@ function App() {
         </Typography>
         <Grid container>
           {[
-            [
-            "Web Development",
-            "Mobile Development",
-            "UI/UX Design",
-            "Data Science",
-            "Data Analytics",
-            "Data Management",
-            'Servers & Infrastructure',
-          ],
-          [
-            "Data Engineering",
-            "Data Visualization",
-            "Automation",
-            "AI & Machine Learning",
-            "Software Engineering",
-            "Software Consultation",
-            "Business Intelligence",
-            "Database Management",
-          ]
-        ].map((array, index) => (
-            <Grid item xs={12} sm={6} key={index}>
-              {array.slice(0, 4).map(item => (
-                <ListItem >
-                  <ListItemText primary={item} secondary={item} />
-                </ListItem>
-              ))}
+            {
+              primary: "Web Development",
+              secondary: "Websites - Web Apps - Servers & Infrastructure"
+            },
+            {
+              primary: "Mobile Development",
+              secondary: "Mobile Apps"
+            },
+            {
+              primary: "UI / UX Design",
+              secondary: "User first design"
+            }
+          ].map((service, index) => (
+            <Grid item xs={12} key={index}>
+              <ListItem >
+                <ListItemText {...service} />
+              </ListItem>
             </Grid>
-          ))
-        }
+          ))}
         </Grid>
       </List>
       <Divider />
@@ -121,10 +300,8 @@ function App() {
           <Box sx={{ display: "flex", gap: 2, mt: 2 }}>
             <Button 
               variant="contained" 
-              color="primary" 
-              href="https://calendly.com/woodward-michael-a/15min"
-              target='_blank'
-              rel="noreferrer"
+              color="primary"
+              onClick={handleSchedule}
             >
               Schedule a Demo
             </Button>
@@ -135,7 +312,7 @@ function App() {
               target='_blank'
               rel="noreferrer"
             >
-              See Projects Board
+              See Demo Project
             </Button>
           </Box>
         </Grid>
@@ -149,8 +326,8 @@ function App() {
             <Container maxWidth="sm" key={i}>
               {post.image && 
                 <img src={post.image} alt="FamilyApps" style={{ 
-                  height: 780, 
-                  width: 340, 
+                  height: 420,
+                  width: 200, 
                   borderRadius: 14, 
                   border: '8px solid #333', 
                   marginTop: 24 
@@ -161,15 +338,33 @@ function App() {
                 {post.title}
               </Typography>
               <Divider />
-              <Typography variant="body1">
+              <Typography variant="body1" textAlign={"left"}>
                 {post.body}
               </Typography>
-              <Typography variant="subtitle2" gutterBottom>
-                2/21/2024
-              </Typography>
-              <Button variant="outlined" color="primary" size='small' mb={2}>
-                Read More
-              </Button>
+              <Box textAlign={"right"}>
+                <Typography variant="subtitle2" gutterBottom>
+                  Wednesday, Februrary 21, 2024
+                </Typography>
+                <Box sx={{ display: "flex", justifyContent: post.type.includes('project') ? "space-between" : "right" }}>
+                  {post.type.includes('project') && (
+                    <Box>
+                      <Tooltip title="Source Code">
+                        <IconButton href={post.github} target='_blank' noreferrer>
+                          <GitHubIcon />
+                        </IconButton>
+                      </Tooltip>
+                      <Tooltip title="View Demo">
+                        <IconButton href={post.url} target='_blank' noreferrer>
+                          <LaunchIcon />
+                        </IconButton>
+                      </Tooltip>
+                    </Box>
+                  )}
+                  <Button variant="outlined" color="primary" onClick={() => handleReadMore(post)}>
+                    Read More
+                  </Button>
+                </Box>
+              </Box>
             </Container>
           ))}
         </Grid>
@@ -196,7 +391,7 @@ function App() {
             <Avatar src={headshot} sx={{ height: '100px', width: '100px' }} />
           <Box>
             <Typography variant="h2">Michael Woodward</Typography>
-            <Typography variant="h4">Software Developer</Typography>
+            <Typography variant="h4">Web Developer</Typography>
             <Typography variant="subtitle1">JavaScript, React, Node, SQL</Typography>
             <Typography variant="h6">Chicago, Illinois, USA</Typography>
             <Typography variant="body1">
@@ -206,7 +401,7 @@ function App() {
               (555) 555-5555
             </Typography>
             <Box>
-              <IconButton sx={{ color: "#fff"}}>
+              <IconButton sx={{ color: "#fff"}} href="https://www.linkedin.com/in/michaelanthonywoodward/" target="_blank" rel="noreferrer">
                 <FaLinkedin />
               </IconButton>
               <IconButton sx={{ color: "#fff"}}>
--- src/utilities/cms.js
+++ src/utilities/cms.js
@@ -10,28 +10,38 @@ export const cms = {
                     body: `The purpose for building myFitness was to build an app that I can use on a daily basis to replace some of the more main stream apps that are available in that space.
                     The reason for doing this was to be able to store my own data in my own database as well as include additional amd custom features to the app.
                     A future goal for the app is to continue to use its foundation and architecture but integrate with other services such as some of the emerging AI technologies and tools becoming available every day. Will include a full tutorial for front end and back end.`,
-                    image: openFitnessImage
+                    image: openFitnessImage,
+                    type: 'project',
+                    github: 'https://github.com/loveliiivelaugh/mypal',
+                    url: 'https://openfitness.netlify.app/'
                 },
                 {
                     title: "Building FamilyApps",
                     body: `The purpose for building familyApps was to build an app that I can use as an app store to replace the more main stream apps that my family and I use on a regular basis. The first major app from this series of personalized applications is a chat UI to chat with Open Source LLM and AI projects. Building these apps for the purpose of personalization, privatization, and maintaining ownership of my own data. Will include a full tutorial for front end and back end.`,
-                    image: familyAppsMobilePic
+                    image: familyAppsMobilePic,
+                    type: 'project',
+                    github: 'https://github.com/loveliiivelaugh',
+                    url: 'https://familyapps.netlify.app/'
                 },
                 {
                     title: "Working with AI",
                     body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+                    type: 'post'
                 },
                 {
                     title: "Automating the Way Forward",
                     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    type: 'post'
                 },
                 {
                     title: "Tech Work in the Modern World",
                     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    type: 'post'
                 },
                 {
                     title: "Apple Shortcuts for Automation",
                     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    type: 'post'
                 },
             ]
         }
