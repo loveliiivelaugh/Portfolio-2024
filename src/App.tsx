badb3368aee2dcfe38acbb796b0afdc945eacb0e
M <woodward.michael.a@gmail.com>
2024-06-18 10:47:33 -0500

    Fixed ts error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { 
   InputAdornment, TextField, Box, Grid, 
   IconButton, Typography, Autocomplete,
