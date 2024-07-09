2d89d7f99402b0a209e61390dde8e2b4439b2194
M <woodward.michael.a@gmail.com>
2024-07-09 07:35:08 -0500

    Added regenerator-runtime package

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Providers } from './Providers/Providers';
 import App from './components/App/App'
+import 'regenerator-runtime'
 import './index.css';
 
 
