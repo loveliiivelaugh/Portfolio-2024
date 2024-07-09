83dbd7e80061e12ad56fe5361cedbf04ff88f43e
M <woodward.michael.a@gmail.com>
2024-07-09 07:43:15 -0500

    Working on regeneratorRuntime problem

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Providers } from './Providers/Providers';
-import App from './components/App/App'
+// import App from './components/App/App'
 import 'regenerator-runtime'
 import './index.css';
 
@@ -10,7 +10,8 @@ createRoot(document.getElementById('root')!)
   .render(
     <StrictMode>
       <Providers>
-        <App />
+        {/* <App /> */}
+        App
       </Providers>
     </StrictMode>,
   );
